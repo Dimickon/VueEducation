e7fcac0f26cb3b1c71bae6e2b1ff44b8e91b7fb2
Dimickon <dimickon1@gmail.com>
2022-07-01 18:00:38 +0300

    add notes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-const App = {
+const Counter = {
     data() {
         return {
             title: 'Счётчик',
@@ -7,6 +7,58 @@ const App = {
     }
 }
 
+const Notes = {
+    data() {
+        return {
+            title: 'Список заметок',
+            notesPlaceholder: 'Введите название заметки',
+            notesValue: '',
+            notes: ['Заметка 1', 'Заметка 2']
+        }
+    },
 
-Vue.createApp(App).mount('#app');
+    methods: {
+        addNewNote() {
+            if (this.notesValue === '') {
+
+            }
+            else {
+                this.notes.push(this.notesValue)
+                this.notesValue = ''
+            }
+        },
+
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+
+        removeNote(index) {
+            this.notes.splice(index, 1)
+        }
+    },
+
+    watch: {
+        notesValue(value) {
+            if(value.length > 20) {
+                this.notesValue = ''
+            }
+        }
+    }
+}
+
+
+Vue.createApp(Counter).mount('#counter');
+Vue.createApp(Notes).mount('#notes');
+
+
+
+
+
+
+
+
+
+
+
+
 
